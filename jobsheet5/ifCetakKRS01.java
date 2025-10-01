c46185c753c65f3fdbbdf6f31c1c6efe0a27c4f7
MamankSYQ <abdurrahmansyafiq151123@gmail.com>
2025-10-01 08:26:06 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS01.java
+++ jobsheet5/ifCetakKRS01.java
@@ -11,5 +11,8 @@ public class ifCetakKRS01 {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KSR dan minta tanda tangan DPA");
         }
+        else {
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
+        }
     }
 }
